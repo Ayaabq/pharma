95a5bdb365b55d92361bd5509513ab367d37d93a
unknown <alzahabe2000@gmail.com>
2023-12-28 12:32:18 +0300

    adding Order Details Screen

---
+++ lib/screens/order_details_screen.dart
@@ -0,0 +1,154 @@
+import 'package:flutter/material.dart';
+import 'package:pharma/models/order_details_model.dart';
+import 'package:pharma/screens/order_screen.dart';
+
+// class OrderDetailsScreen extends StatelessWidget {
+//   OrderDetailsScreen({super.key, required this.dr});
+//   Data dr;
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Order Details'),
+//         backgroundColor: Color.fromARGB(255, 67, 201, 201),
+//       ),
+//       body: ListView.builder(
+//         itemCount: dr.products?.length,
+//         padding: const EdgeInsets.symmetric(horizontal: 5),
+//         itemBuilder: (ctx, index) => OrderDetailsList(
+//           pr: dr.products!,
+//
+//           // onSelectedMedicine: _selectMedicine(context),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+// class OrdersScreen extends StatefulWidget {
+//   OrderScreen({super.key});
+//   @override
+//   _OrdersScreenState createState() => _OrdersScreenState();
+// }
+//
+// class _OrdersScreenState extends State<OrdersScreen> {
+//   List<Data>? ordersData = []; // Replace with actual data
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Orders'),
+//       ),
+//       body: ListView.builder(
+//         itemCount: ordersData?.length ?? 0,
+//         itemBuilder: (context, index) {
+//           return ListTile(
+//             title: Text('Order #${ordersData![index].id}'),
+//             subtitle: Text('Status: ${ordersData![index].status}'),
+//             onTap: () {
+//               Navigator.push(
+//                 context,
+//                 MaterialPageRoute(
+//                   builder: (context) => ProductsDetailsScreen(
+//                     orderData: ordersData![index],
+//                   ),
+//                 ),
+//               );
+//             },
+//           );
+//         },
+//       ),
+//     );
+//   }
+// }
+
+// class OrdersScreen extends StatefulWidget {
+//   OrdersScreen({super.key, required this.ordersData});
+//   List<Data> ordersData;
+//   @override
+//   State<OrdersScreen> createState() => _OrdersScreenState();
+// }
+//
+// class _OrdersScreenState extends State<OrdersScreen> {
+//   // Replace with actual data
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Orders'),
+//       ),
+//       body: ListView.builder(
+//         itemCount: widget.ordersData?.length ?? 0,
+//         itemBuilder: (context, index) {
+//           return ListTile(
+//             title: Text('Order #${widget.ordersData![index].id}'),
+//             subtitle: Text('Status: ${widget.ordersData![index].status}'),
+//             onTap: () {
+//               // Navigator.push(
+//               //   context,
+//               //   MaterialPageRoute(
+//               //     builder: (context) => ProductsDetailsScreen(
+//               //       orderData: ordersData![index],
+//               //     ),
+//               //   ),
+//               // );
+//             },
+//           );
+//         },
+//       ),
+//     );
+//   }
+// }
+
+class ProductsDetailsScreen extends StatelessWidget {
+  const ProductsDetailsScreen({super.key, required this.orderData});
+  final Data orderData;
+  @override
+  Widget build(BuildContext context) {
+    // num totalPrice = orderData.products?.fold(
+    //     0, (sum, product) => sum + product.quantityAvailable! * product.cost!);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Order Details'),
+        backgroundColor: Color.fromARGB(255, 67, 201, 201),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          ListTile(
+            title: Text('Status: ${orderData.status}'),
+          ),
+          ListTile(
+            title: Text('Payment Status: ${orderData.paymentStatus}'),
+          ),
+          ListTile(
+            title: Text('Total Price: ${orderData.orderPrice}'),
+          ),
+          Divider(),
+          Text('Products:'),
+          Expanded(
+            child: ListView.builder(
+              itemCount: orderData.products?.length ?? 0,
+              itemBuilder: (context, index) {
+                Products product = orderData.products![index];
+                return ListTile(
+                  title: Text(product.commercialName!),
+                  subtitle: Text('Quantity: ${product.pivot!.quantity}'),
+                  trailing: Text(
+                      'Price: ${product.pivot!.quantity! * product.cost!}'),
+                );
+              },
+            ),
+          ),
+          // ListTile(
+          //   title: Text('Total Price: ${orderData.orderPrice}'),
+          // ),
+        ],
+      ),
+    );
+    ;
+  }
+}
--- lib/widgets/order_list.dart
+++ lib/widgets/order_list.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:get/get.dart';
 import 'package:pharma/models/order_details_model.dart';
+import 'package:pharma/screens/order_details_screen.dart';
 import 'package:pharma/services/get_order_details_service.dart';
 
 import '../providers/auth_data_provider.dart';
@@ -32,63 +33,84 @@ class _OrderListState extends ConsumerState<OrderList> {
               List<Data> orders = snapshot.data!;
               return Expanded(
                 child: ListView.builder(
-                  itemCount: orders.length,
+                  itemCount: orders?.length ?? 0,
+                  //itemCount: orders.length,
                   // padding: const EdgeInsets.all(),
                   itemBuilder: (ctx, index) {
-                    Data order = orders[index];
+                    //Data order = orders[index];
 
-                    return Card(
-                      elevation: 5,
-                      margin: EdgeInsets.symmetric(horizontal: 8, vertical: 8),
-                      child: Padding(
-                        padding: EdgeInsets.fromLTRB(
-                          15,
-                          25,
-                          25,
-                          25,
-                        ),
-                        // EdgeInsets.symmetric(horizontal: 10, vertical: 25),
-                        child: Stack(
-                          children: [
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  'Order: ${index + 1}',
-                                  style: TextStyle(
-                                    fontSize: 18,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                                // Add other order details as needed...
-                              ],
+                    return InkWell(
+                      onTap: () {
+                        // print('object');
+                        Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (ctx) => ProductsDetailsScreen(
+                              orderData: orders![index],
                             ),
-                            Positioned(
-                              right: 0,
-                              bottom: 0,
-                              top: 0,
-
-                              //right: 0,
-                              // child:
-                              // Container(
-                              //   padding: EdgeInsets.all(8),
-                              //   decoration: BoxDecoration(
-                              //     color: Colors.grey,
-                              //     borderRadius: BorderRadius.circular(12),
-                              //   ),
-
-                              child: Row(
-                                mainAxisSize: MainAxisSize.min,
+                          ),
+                        );
+                      },
+                      child: Card(
+                        elevation: 5,
+                        margin:
+                            EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+                        child: Padding(
+                          padding: EdgeInsets.fromLTRB(
+                            15,
+                            25,
+                            25,
+                            25,
+                          ),
+                          // EdgeInsets.symmetric(horizontal: 10, vertical: 25),
+                          child: Stack(
+                            children: [
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
                                 children: [
                                   Text(
-                                    order.status.toString(),
-                                    //style: TextStyle(color: Colors.white),
+                                    'Order# ${index + 1}',
+                                    style: TextStyle(
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.bold,
+                                    ),
                                   ),
+                                  Text(
+                                    'Price: ${orders![index].orderPrice.toString()}',
+                                    style: TextStyle(
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  // Add other order details as needed...
                                 ],
                               ),
-                              // ),
-                            ),
-                          ],
+                              Positioned(
+                                right: 0,
+                                bottom: 0,
+                                top: 0,
+
+                                //right: 0,
+                                // child:
+                                // Container(
+                                //   padding: EdgeInsets.all(8),
+                                //   decoration: BoxDecoration(
+                                //     color: Colors.grey,
+                                //     borderRadius: BorderRadius.circular(12),
+                                //   ),
+
+                                child: Row(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    Text(
+                                      orders![index].status.toString(),
+                                      //style: TextStyle(color: Colors.white),
+                                    ),
+                                  ],
+                                ),
+                                // ),
+                              ),
+                            ],
+                          ),
                         ),
                       ),
                     );
